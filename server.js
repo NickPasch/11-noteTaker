1e9ff58718171617b01978ba033145720a817316
Nick Pasch <54225021+NickPasch@users.noreply.github.com>
2020-12-10 18:51:59 -0700

    adding comments
    adding comments manually

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
+// All requirements 
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const notes = require("./db/db.json");
 
 var app = express();
-
+// Establishing the port
 var PORT = process.env.PORT || 8080;
-
+// Ability to decipher all required data
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static('public'));
-
+// Home page path
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 })
-
+// Note page path 
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 })
-
+// JSON of note data
 app.get("/api/notes", (req, res) => {
     fs.readFile("./db/db.json", "utf-8", (err, data) => {
         return res.json(JSON.parse(data));
     })
 })
-
+// Convert user input to JSON and add to data
 app.post("/api/notes", (req, res) =>{
     console.log(req.body);
     req.body["id"] = notes.length + 1;
@@ -34,7 +35,7 @@ app.post("/api/notes", (req, res) =>{
         return res.json(req.body);
     })
 })
-
+// When the delete button is clicked, delete the note with the trashcan's ID
 app.delete("/api/notes/:id", (req, res) =>{
     let ID = req.params.id;
     for(i=0;i<notes.length;i++){
